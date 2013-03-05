45e35b01203bf76fb4db2c98a0b2591b88d07f45
unknown <JterVoorde@ovspc262.OVSoftware.local>
2013-03-05 15:49:36 +0100

    Fixes

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-export MAVEN_OPTS="-Xmx768m -XX:MaxPermSize=256m -Dmaven.repo.local=repo"
+set MAVEN_OPTS=-Xmx768m -XX:MaxPermSize=256m -Dmaven.repo.local=".\repo"
 mvn -Dmaven.test.skip=true clean install
--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 
-set MAVEN_OPTS=-Xmx1024m -XX:MaxPermSize=256m -Dmaven.repo.local=repo -agentpath:"C:\Program Files (x86)\YourKit Java Profiler 12.0.3\bin\win64\yjpagent.dll=triggers=triggers.txt,delay=20000,dir=snapshots"
+set MAVEN_OPTS=-Xmx1024m -XX:MaxPermSize=256m -Dmaven.repo.local=".\repo" -agentpath:"C:\Program Files (x86)\YourKit Java Profiler 12.0.3\bin\win64\yjpagent.dll=triggers=triggers.txt,delay=20000,dir=snapshots"
 mvn -Dmaven.test.skip=true clean install embedded-glassfish:run
